2a76c0b8691df155ce4cc9fef9ce954e6ddb41e7
Chrzddd <enzopinheiro6@gmail.com>
2023-02-07 00:49:58 -0300

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,9 @@ import topRightImageFrame from '../../../public/imagers/topRightImageFrame.png'
 import bottomLeftImageFrame from '../../../public/imagers/bottomLeftImageFrame.png'
 import topImageFrameMobile from '../../../public/imagers/topImageFrameMobile.png'
 import bottomImageFrameMobile from '../../../public/imagers/bottomImageFrameMobile.png'
+import { BiLockAlt } from 'react-icons/bi'
+import { MdOutlineMailOutline } from 'react-icons/md'
+
 export default function Login() {
   return (
     <div className={styles.login}>
@@ -22,8 +25,30 @@ export default function Login() {
       <div className={styles.centerContainer}>
         <div className={styles.boxCenterContainers}>
           <div className={styles.leftBox}></div>
-          <div className={styles.rightBox}></div>
+          <div className={styles.rightBox}>
+            <div className={styles.inputStylesBoxs}>
+              <input
+                type="text"
+                className={styles.usersInputBox}
+                placeholder="Email:"
+              />
+              <span className={styles.i}>
+                <MdOutlineMailOutline width={15} height={19} />
+              </span>
+            </div>
+            <div className={styles.inputStylesBoxs}>
+              <input
+                type="text"
+                className={styles.usersInputBox}
+                placeholder={`Senha:`}
+              />
+              <span className={styles.i}>
+                <BiLockAlt width={15} height={19} />
+              </span>
+            </div>
+          </div>
         </div>
+
         <div className={styles.bottomContainerImages}>
           <Image
             className={styles.bottomLeftImageFrame}
