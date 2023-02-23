539f1f6b86f012df55f2620242e7f2c822f3b8e6
Chrzddd <enzopinheiro6@gmail.com>
2023-02-23 01:21:05 -0300

    update

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,27 +1,62 @@
 import styles from './Table.module.scss'
+import { useState } from 'react'
+import { Users } from '../../services/userServices'
+
 export default function Table() {
+  const [itens, setItens] = useState(Users)
+  const [itensPerPage, setItensPerPage] = useState(8)
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const startIndex = currentPage * itensPerPage
+  const endIndex = startIndex + itensPerPage
+  const pages = Math.ceil(itens.length / itensPerPage)
+
+  const currentItens = itens.slice(startIndex, endIndex)
+
   return (
-    <table className={styles.tableContainer}>
-      <tr>
-        <th>Identificação</th>
-        <th>RG</th>
-        <th>CPF</th>
-        <th>Celular</th>
-      </tr>
-      <tbody className={styles.tbodyteste}>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-          <td>Germany</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className={styles.tableContainer}>
+      <div className={styles.tableSection}>
+        <table>
+          <tr>
+            <th className={styles.nameIdentify}>Identificação</th>
+            <th className={styles.nameIdentify}>RG</th>
+            <th className={styles.nameIdentify}>CPF</th>
+            <th className={styles.nameIdentify}>Celular</th>
+          </tr>
+          <tbody>
+            {currentItens.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.rg}</td>
+                  <td>{item.cpf}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.footerPagination}>
+        <p>Mostrando 1 - 8 de {itens.length} cadastros</p>
+        <div className={styles.paginationsFooterSection}>
+          <button
+            disabled={currentPage === 0}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            {`<`}
+          </button>
+          <div>
+            <p>{currentPage + 1}</p>
+          </div>
+          <button
+            disabled={currentPage + 1 === pages}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            {`>`}
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -2,36 +2,79 @@ export const Users = [
   {
     id: 1,
     name: 'Carolina',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar:
       'https://i0.wp.com/techwek.com/wp-content/uploads/2020/12/bem-aleatoria-essa-foto-de-gato..jpg?resize=783%2C1024&ssl=1',
   },
-  { id: 2, name: 'Enzo Leão', avatar: 'https://github.com/enzoleao.png' },
+  {
+    id: 2,
+    name: 'Enzo Leão',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
+    avatar: 'https://github.com/enzoleao.png',
+  },
   {
     id: 3,
     name: 'Leonardo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar: 'https://github.com/LeonardoSilvaCosta.png',
   },
   {
     id: 4,
     name: 'Mel',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar:
       'https://i.pinimg.com/736x/04/2d/86/042d862deed61117539765328f8bd719.jpg',
   },
   {
     id: 5,
     name: 'Enzo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8Ea9cyIi_U8y7mgzqX1SitKtppQOzlciXA&usqp=CAU',
   },
   {
     id: 6,
     name: 'Enzo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8Ea9cyIi_U8y7mgzqX1SitKtppQOzlciXA&usqp=CAU',
   },
   {
     id: 7,
     name: 'Enzo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
+    avatar:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8Ea9cyIi_U8y7mgzqX1SitKtppQOzlciXA&usqp=CAU',
+  },
+  {
+    id: 8,
+    name: 'Enzo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
+    avatar:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8Ea9cyIi_U8y7mgzqX1SitKtppQOzlciXA&usqp=CAU',
+  },
+  {
+    id: 9,
+    name: 'Enzo',
+    rg: 123123,
+    cpf: '123.123.213-21',
+    phone: '(91) 99161-1201',
     avatar:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8Ea9cyIi_U8y7mgzqX1SitKtppQOzlciXA&usqp=CAU',
   },
